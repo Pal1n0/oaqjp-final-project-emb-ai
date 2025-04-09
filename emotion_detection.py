f0a9106b77a1ca9dcc8a8a9ad982a07b641ee82b
Pal1n0 <pavoldzama@gmail.com>
2025-04-09 10:35:36 -0400

    Updated return - dict of emotion probab.

--- emotion_detection.py
+++ emotion_detection.py
@@ -11,7 +11,33 @@ def emotion_detector(text_to_analyse):
 
     response = requests.post(url, json=json_input ,headers = headers, timeout = 10)
 
-    return response.text
+    json_response = response.json()
+
+    max_score = 0
+    max_score_emotion = None
+
+    for emotion, score in json_response["emotionPredictions"][0]["emotion"].items():
+        if score > max_score:
+            max_score = score
+            max_score_emotion = emotion
+
+    anger_score = json_response["emotionPredictions"][0]["emotion"]["anger"]
+    disgust_score = json_response["emotionPredictions"][0]["emotion"]["disgust"]
+    fear_score = json_response["emotionPredictions"][0]["emotion"]["fear"]
+    joy_score = json_response["emotionPredictions"][0]["emotion"]["joy"]
+    sadness_score = json_response["emotionPredictions"][0]["emotion"]["sadness"]
+
+    emotion_results = (
+    {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': max_score_emotion
+    }
+    )
+    return emotion_results
 
 
 
