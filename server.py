f42c05a0fb12ce6dd591196e4f36a50fca0a5d8a
Pal1n0 <pavoldzama@gmail.com>
2025-04-09 11:20:34 -0400

    Server fully defined

--- server.py
+++ server.py
@@ -8,11 +8,19 @@ def homescreen():
     return render_template("index.html")
 
 @app.route("/emotionDetector")
-def emotion_detection(text_to_analyse):
+def emotion_detection():
+    text_to_analyse = request.args.get("textToAnalyze")
     output = emotion_detector(text_to_analyse)
-    return output
+
+    dominant_emotion = output["dominant_emotion"]
+    anger = output["anger"]
+    disgust = output["disgust"]
+    fear = output["fear"]
+    joy = output["joy"]
+    sadness = output["sadness"]
+    
+    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear':{fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
